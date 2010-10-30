0b0b862df9f90568b7a4bbd5cc1c50feaf9d3ed1
Marcus Ekelund <marcus.ekelund@gmail.com>
2010-10-30 12:10:24 +0200

    added some docstrings

--- pyopenkeyval.py
+++ pyopenkeyval.py
@@ -1,3 +1,13 @@
+"""
+A simple interface to OpenKeyval.org
+
+OpenKeyval.org is a service for easily storing and retrieving key/value pairs
+via HTTP. pyopenkeyval is a Python interface to this service, presented as a
+dict-like object that accesses data on OpenKeyval.
+
+Website: http://github.com/marcuse/pyopenkeyval
+"""
+
 import urllib
 import urllib2
 try:
@@ -8,6 +18,8 @@ except ImportError:
 __version__ = '0.1'
 
 class pyopenkeyval(object):
+    """A dict-like object that can read and write values to keys stored on OpenKeyval.org"""
+
     _api_url = 'http://api.openkeyval.org/%s'
 
     def __getitem__(self, key):
@@ -27,6 +39,7 @@ class pyopenkeyval(object):
         self.store(key, '')
 
     def store(self, key, value):
+        """Stores `value` on `key`, returns a dict of the parsed JSON response."""
         data = urllib.urlencode({'data': value})
         result = urllib2.urlopen(self._api_url % key, data).read()
         return json.loads(result)
